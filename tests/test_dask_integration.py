2e875bd46b7bf61a676b0643f8cfe3bdde89c12c
Igor <igor.niebylski@gmail.com>
2025-05-26 12:18:13 +0200

    tests

---
+++ tests/test_dask_integration.py
@@ -0,0 +1,88 @@
+import math
+import pytest
+import logging
+import conftest
+from utils import *
+import dask.dataframe as dd
+import qdb_dask_connector as qdbdsk
+from dask.distributed import LocalCluster, Client
+
+logger = logging.getLogger("test-dask-integration")
+
+
+@conftest.override_cdtypes([np.dtype("float64")])
+@pytest.mark.parametrize("row_count", [224], ids=["row_count=224"], indirect=True)
+@pytest.mark.parametrize("sparsify", [100], ids=["sparsify=none"], indirect=True)
+def test_dask_query_meta_set(df_with_table, qdbd_connection, qdbd_settings):
+    """
+    tests that the columns are set correctly in the dask DataFrame
+    """
+    _, _, query = prepare_query_test(df_with_table, qdbd_connection)
+    df = qdbpd.query(qdbd_connection, query)
+    ddf = qdbdsk.query(query, qdbd_settings.get("uri").get("insecure"))
+
+    dask_meta = ddf._meta_nonempty
+    pandas_cols = df.columns
+
+    assert dask_meta.columns.names == pandas_cols.names, "column names do not match"
+
+    for col_name in pandas_cols:
+        # treat string[pyarrow] as object
+        if dask_meta[col_name].dtype == "string[pyarrow]":
+            dask_meta[col_name] = dask_meta[col_name].astype("object")
+
+        assert (
+            dask_meta[col_name].dtype == df[col_name].dtype
+        ), f"dtype of column {col_name} does not match"
+
+
+@conftest.override_cdtypes([np.dtype("float64")])
+@pytest.mark.parametrize("row_count", [224], ids=["row_count=224"], indirect=True)
+@pytest.mark.parametrize("sparsify", [100], ids=["sparsify=none"], indirect=True)
+def test_dask_query_lazy_evaluation(df_with_table, qdbd_connection, qdbd_settings):
+    """
+    tests that the function is lazy and does not return a Dataframe immediately.
+    """
+
+    _, _, query = prepare_query_test(df_with_table, qdbd_connection)
+
+    ddf = qdbdsk.query(query, qdbd_settings.get("uri").get("insecure"))
+
+    assert isinstance(ddf, dd.DataFrame)
+    result = ddf.compute()
+    assert isinstance(result, pd.DataFrame)
+
+
+@conftest.override_cdtypes([np.dtype("float64")])
+@pytest.mark.parametrize("row_count", [224], ids=["row_count=224"], indirect=True)
+@pytest.mark.parametrize("sparsify", [100], ids=["sparsify=none"], indirect=True)
+@pytest.mark.parametrize("frequency", ["h"], ids=["frequency=H"], indirect=True)
+def test_dask_query_parallelized(df_with_table, qdbd_connection, qdbd_settings):
+    _, _, df, table = df_with_table
+    shard_size = table.get_shard_size()
+    start, end = df.index[0], df.index[-1]
+
+    _, _, query = prepare_query_test(df_with_table, qdbd_connection)
+
+    ddf = qdbdsk.query(query, qdbd_settings.get("uri").get("insecure"))
+
+    # value of npartitions determines number of delayed tasks
+    # delayed tasks can be executed in parallel
+    # currently tasks are created for each shard
+    expected_number_of_partitions = math.ceil(
+        (end - start).total_seconds() / shard_size.total_seconds()
+    )
+    assert ddf.npartitions == expected_number_of_partitions
+
+
+@conftest.override_cdtypes([np.dtype("float64")])
+@pytest.mark.parametrize("row_count", [224], ids=["row_count=224"], indirect=True)
+@pytest.mark.parametrize("sparsify", [100], ids=["sparsify=none"], indirect=True)
+def test_dask_compute_on_local_cluster(df_with_table, qdbd_connection, qdbd_settings):
+    _, _, query = prepare_query_test(df_with_table, qdbd_connection)
+
+    with LocalCluster(n_workers=2) as cluster:
+        with Client(cluster):
+            ddf = qdbdsk.query(query, qdbd_settings.get("uri").get("insecure"))
+            res = ddf.compute()
+            res.head()
---
+++ tests/utils.py
@@ -0,0 +1,82 @@
+import logging
+import numpy as np
+import numpy.ma as ma
+import pandas as pd
+import quasardb.pandas as qdbpd
+
+
+def _to_numpy_masked(xs):
+    data = xs.to_numpy()
+    mask = xs.isna()
+    return ma.masked_array(data=data, mask=mask)
+
+
+def _assert_series_equal(lhs, rhs):
+    lhs_ = _to_numpy_masked(lhs)
+    rhs_ = _to_numpy_masked(rhs)
+
+    assert ma.count_masked(lhs_) == ma.count_masked(rhs_)
+
+    logging.debug("lhs: %s", lhs_[:10])
+    logging.debug("rhs: %s", rhs_[:10])
+
+    lhs_ = lhs_.torecords()
+    rhs_ = rhs_.torecords()
+
+    for (lval, lmask), (rval, rmask) in zip(lhs_, rhs_):
+        assert lmask == rmask
+
+        if lmask is False:
+            assert lval == rval
+
+
+def assert_df_equal(lhs, rhs):
+    """
+    Verifies DataFrames lhs and rhs are equal(ish). We're not pedantic that we're comparing
+    metadata and things like that.
+
+    Typically one would use `lhs` for the DataFrame that was generated in code, and
+    `rhs` for the DataFrame that's returned by qdbpd.
+    """
+
+    np.testing.assert_array_equal(lhs.index.to_numpy(), rhs.index.to_numpy())
+    assert len(lhs.columns) == len(rhs.columns)
+    for col in lhs.columns:
+        _assert_series_equal(lhs[col], rhs[col])
+
+
+def prepare_query_test(
+    df_with_table,
+    qdbd_connection,
+    columns: str = "*",
+    query_range: tuple[pd.Timestamp, pd.Timestamp] = None,
+    group_by: str = None,
+):
+    (_, _, df, table) = df_with_table
+
+    qdbpd.write_dataframe(df, qdbd_connection, table, write_through=True)
+    table_name = table.get_name()
+    q = 'SELECT {} FROM "{}"'.format(columns, table_name)
+
+    if query_range:
+        q += " IN RANGE({}, {})".format(query_range[0], query_range[1])
+
+    if group_by:
+        q += " GROUP BY {}".format(group_by)
+
+    return (df, table, q)
+
+
+def get_subrange(
+    df: pd.DataFrame, slice_size: int = 0.1
+) -> tuple[pd.Timestamp, pd.Timestamp]:
+    """
+    Returns slice of the Dataframe index to be used in the query.
+    """
+    query_range = ()
+    if slice_size != 1:
+        start_str = df.index[0].to_pydatetime().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        end_row = int((len(df) - 1) * slice_size)
+        end_str = df.index[end_row].to_pydatetime().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        query_range = (start_str, end_str)
+    return query_range
