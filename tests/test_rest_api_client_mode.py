2ebea62ac07982d81966592e5b0991475d49df2e
Igor <igor.niebylski@gmail.com>
2025-05-26 22:13:28 +0200

    fix build process

--- tests/test_rest_api_client_mode.py
+++ tests/test_rest_api_client_mode.py
@@ -5,8 +5,10 @@ import logging
 logger = logging.getLogger("test-dask-rest-api-client-mode")
 
 
-def test_client_in_rest_mode():
+def test_client_in_rest_mode_not_implemented_yet():
     with pytest.raises(NotImplementedError):
-        df = qdb_dask_connector.query(
-            "select * from test", "qdb://127.0.0.1:2836", rest_uri="localhost:40080"
+        qdb_dask_connector.query(
+            "select * from test",
+            cluster_uri="qdb://127.0.0.1:2836",
+            rest_uri="localhost:40080",
         )
