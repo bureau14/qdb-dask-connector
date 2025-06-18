c96ca4996643e23b113c0be4b4c78479f585330e
Igor <igor.niebylski@gmail.com>
2025-06-18 14:54:19 +0200

    add test for lazy dataframes

--- tests/test_write_dataframe.py
+++ tests/test_write_dataframe.py
@@ -1,7 +1,5 @@
-import pytest
 import quasardb_dask as qdbdask
 import logging
-import dask
 from utils import assert_df_equal
 import quasardb.pandas as qdbpd
 from dask.delayed import Delayed
@@ -10,23 +8,24 @@ from dask.delayed import Delayed
 logger = logging.getLogger("test-write-dataframe")
 
 
-def test_write_dataframe_is_lazy(df_with_table, qdbd_settings):
+def test_write_dataframe_is_lazy(df_with_table, qdbd_connection, qdbd_settings):
     _, _, df, table = df_with_table
     table_name = table.get_name()
 
     write_task = qdbdask.write_dataframe(
-        df, cluster_uri=qdbd_settings.get("uri").get("insecure"), table_name=table_name
+        df,
+        cluster_uri=qdbd_settings.get("uri").get("insecure"),
+        table_name=table_name,
+        create=False,
     )
 
     assert (
         type(write_task) == Delayed
     ), "write_dataframe should return a dask delayed object"
+    assert qdbpd.read_dataframe(qdbd_connection, table).empty()
 
 
-def test_write_dataframe_from_pandas_df(df_with_table, qdbd_connection, qdbd_settings):
-    """
-    Verify that the written dataframe matches the original pandas dataframe.
-    """
+def test_write_pandas_dataframe(df_with_table, qdbd_connection, qdbd_settings):
     _, _, df, table = df_with_table
     table_name = table.get_name()
 
@@ -40,6 +39,28 @@ def test_write_dataframe_from_pandas_df(df_with_table, qdbd_connection, qdbd_set
     assert_df_equal(df, written_df)
 
 
-def test_write_dataframe_from_dask_df(df_with_table, qdbd_connection, qdbd_settings):
-    _, _, df, table = df_with_table
-    pass
+def test_write_dask_dataframe(df_with_table_inserted, qdbd_connection, qdbd_settings):
+    df, table = df_with_table_inserted
+
+    # Query back using dask connector
+    original_table_name = table.get_name()
+    ddf = qdbdask.query(
+        f'SELECT * FROM "{original_table_name}"',
+        cluster_uri=qdbd_settings.get("uri").get("insecure"),
+    )
+    assert isinstance(ddf, Delayed), "Expected a dask delayed object"
+
+    # Write delayed dataframe to a new table
+    new_table_name = f"{original_table_name}_COPY"
+    qdbdask.write_dataframe(
+        ddf,
+        table_name=new_table_name,
+        cluster_uri=qdbd_settings.get("uri").get("insecure"),
+    ).compute()
+
+    # Read back from new table and compare with source
+    written_df = qdbpd.read_dataframe(
+        qdbd_connection, qdbd_connection.table(new_table_name)
+    )
+
+    assert_df_equal(df, written_df)
